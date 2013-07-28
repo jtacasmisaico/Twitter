eea3838e3ce6cfac4b987b7a0ebef047610cdcf2
Vishnu <vishnumohandas@gmail.com>
2013-07-28 20:13:11 +0530

    Cleaned up code

--- website/proxy.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $curl = curl_init();
-    curl_setopt ($curl, CURLOPT_URL, "http://localhost:8080/users/followers/4");
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-    $result = curl_exec ($curl);
-    curl_close ($curl);
-    print $result;
-?>
