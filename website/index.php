46bdb09ff0ac26fac8d7eb5abbca10e6140fad05
Vishnu <vishnumohandas@gmail.com>
2013-08-22 12:46:25 +0530

    Scripts are now loaded right at the end

--- website/index.php
+++ website/index.php
@@ -6,18 +6,18 @@
     <link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen">
     <link href="css/twitter.css" rel="stylesheet" media="screen">
     <link href="css/jquery-ui.css" rel="stylesheet" media="screen">
-	<script src="js/jquery.js"></script>
+</head>
+<body>
+	<?include("interfaceLoader.php");?>
+
+
+    <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/bootbox.min.js"></script>
     <script src="js/formValidation.js"></script> 
     <script src="js/jquery-ui.js"></script>
     <script src="js/jquery.timeago.js"></script>
-
     <?include("jsLoader.php");?>
-
     <script src="js/twitter.js"></script>
-</head>
-<body>
-	<?include("interfaceLoader.php");?>
 </body>
 </html>
